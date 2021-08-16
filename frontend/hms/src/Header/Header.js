0f32b9d3bf898889d91c44dc3d09b3d24d12a431
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-08-17 03:06:04 +0530

    Add logo

--- frontend/hms/src/Header/Header.js
+++ frontend/hms/src/Header/Header.js
@@ -5,14 +5,16 @@ const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-dark navbar-expand-md fixed-top bg-dark">
+                {/*<a href="https://icons8.com/icon/44053/star"></a>*/}
                 <div className="container">
                     <button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span
                         className="visually-hidden">Toggle navigation</span><span
                         className="navbar-toggler-icon"></span></button>
                     <div className="collapse navbar-collapse" id="navcol-1">
                         <ul className="navbar-nav flex-grow-1 justify-content-between">
-                            <li className="nav-item"><a className="nav-link" href="#"><i
-                                className="fa fa-apple apple-logo"></i></a></li>
+                            <img src="https://img.icons8.com/nolan/64/star.png"/>
+                            {/*<li className="nav-item"><a className="nav-link" href="#"><i*/}
+                            {/*    className="fa fa-apple apple-logo"></i></a></li>*/}
                             <li className="company-name">Fortune Inn & Suites</li>
                             <li className="nav-item"><a className="nav-link" href="#">Login</a></li>
                             <li className="nav-item"><a className="nav-link" href="#">Sign Up</a></li>
