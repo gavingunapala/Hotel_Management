82fa526d92e089913ed03e6f887f30c9cd1a228a
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-08-17 03:04:23 +0530

    add rooms page updated

--- frontend/hms/src/Room_Management/AddRooms.js
+++ frontend/hms/src/Room_Management/AddRooms.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import img from '../Images/undraw_Sync_files_re_ws4c.png';
+import '../CSS/Room_Management/AddRooms.css';
 
 const AddRooms = () => {
     return (
@@ -10,21 +11,21 @@ const AddRooms = () => {
                 <div className="col-sm-2"></div>
                 <div class=" col-sm-3">
                     <div><strong></strong><label></label></div>
-                    <div class=" justify-content-center align-items-center">
+                    <div className=" justify-content-center align-items-center">
                         <div>
-                            <form method="post" class="card">
+                            <form method="post" className="card">
                                 <br/>
                                 <h2 className="text-center">Add Rooms</h2>
                                 <br/>
                                 <div className="container   ">
-                                    <div class="form-group">
-                                        <div><label>Room Type</label><input class="form-control" type="text"/></div>
-                                        <div><label>Sleeps</label><input class="form-control" type="text"/></div>
-                                        <div><label>Current Price</label><input class="form-control" type="text"/></div>
-                                        <div><label>Facilities</label><input class="form-control" type="text"/></div>
+                                    <div className="form-group">
+                                        <div><label>Room Type</label><input className="form-control" type="text"/></div>
+                                        <div><label>Sleeps</label><input className="form-control" type="text"/></div>
+                                        <div><label>Current Price</label><input className="form-control" type="text"/></div>
+                                        <div><label>Facilities</label><input className="form-control" type="text"/></div>
                                         <div><label>Description</label><textarea id="w3review" className="form-control" rows="4" cols="50"/></div>
                                         <br/>
-                                        <button class="btn btn-primary" type="submit">&nbsp;Add Rooms</button>&nbsp;&nbsp;
+                                        <button className="btn btn-primary" type="submit">&nbsp;Add Rooms</button>&nbsp;&nbsp;
                                         <br/>
                                         <br/>
 
