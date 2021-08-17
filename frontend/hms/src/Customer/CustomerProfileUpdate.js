0f169d1261496f84bdbd24f9a2e264c1768086ae
duhithu <duhithus@gmail.com>
2021-08-17 22:41:23 +0530

    Update, Retrieve and Delete Functions of Customer Profile and Update Form

--- frontend/hms/src/Customer/CustomerProfileUpdate.js
+++ frontend/hms/src/Customer/CustomerProfileUpdate.js
@@ -1,7 +1,78 @@
 import React from 'react';
 import img from '../Images/CustomerProfileUpdate.png';
+import {useHistory} from "react-router-dom";
 
 const CustomerProfile = () => {
+
+    // let his = useHistory();
+    //
+    // const [Customer, setCustomer] = useState([]);
+    //
+    // //get logged admin
+    // useEffect(() => {
+    //     const loggedInUser = localStorage.getItem("user");
+    //     console.log(loggedInUser);
+    //
+    //     function getCustomer() {
+    //         axios.get("http://localhost:8070/customer/get/" + loggedInUser).then((res) => {
+    //             setCustomer(res.data);
+    //             console.log(res.data);
+    //         }).catch((err) => {
+    //         })
+    //     }
+    //
+    //     getCustomer();
+    // }, []);
+    //
+    // //updateOne
+    // const [Name, setName] = useState("");
+    // const [Address, setAddress] = useState("");
+    // const [PhoneNumber, setPhoneNumber] = useState("");
+    // const [NICNumber, setNICNumber] = useState("");
+    // const [Email, setEmail] = useState("");
+    // const [Password, setPassword] = useState("");
+    //
+    //     //customer
+    // const NameSetter = (e) => {
+    //     setName(e.target.value);
+    // }
+    // const AddressSetter = (e) => {
+    //     setAddress(e.target.value);
+    // }
+    // const PhoneNumberSetter = (e) => {
+    //     setPhoneNumber(e.target.value);
+    // }
+    // const NICNumberSetter = (e) => {
+    //     setNICNumber(e.target.value);
+    // }
+    // const EmailSetter = (e) => {
+    //     setEmail(e.target.value);
+    // }
+    // const PasswordSetter = (e) => {
+    //     setPassword(e.target.value);
+    // }
+    //
+    // const onSubmit = () => {
+    //     const newCustomer = {
+    //         Name: Name,
+    //         Address: Address,
+    //         PhoneNumber: PhoneNumber,
+    //         NICNumber: NICNumber,
+    //         Email: Email,
+    //         Password: Password,
+    //     };
+    //
+    //     const loggedInUser = localStorage.getItem("user");
+    //     console.log(loggedInUser);
+    //     axios.put('http://localhost:8070/customer/updateOne/' + loggedInUser, newAttendee).then(() => {
+    //         alert("Updated successfully!!!");
+    //         his.push('/CustomerProfile')
+    //
+    //     }).catch((err) => {
+    //         alert(err);
+    //     })
+    // }
+
     return (
         <div>
             <br></br>
@@ -17,7 +88,7 @@ const CustomerProfile = () => {
                             <form method="post" class="card">
                                 <br />
                                 {/*<h2 class="text-center">Student Registration</h2>*/}
-                                <br />
+
                                 <div className="container   ">
                                     {/*<div><label>Enter First Name</label><input class="form-control" type="text"/>*/}
                                     {/*</div>*/}
