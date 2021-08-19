79dfd7d19c32b287a39a7c0091cde43a7d3ce4cb
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-08-20 00:19:02 +0530

    Add search bar

---
+++ frontend/hms/src/Common/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Search = () => {
+
+    return (
+   <div>
+       <div className="col-xs-6">
+           <div className="searchBar">
+               <i className="fa fa-search"></i>
+               <input type="search" className="form-control" placeholder="Search"/>
+           </div>
+       </div>
+
+   </div>
+       );
+   }
+
+export default Search;
