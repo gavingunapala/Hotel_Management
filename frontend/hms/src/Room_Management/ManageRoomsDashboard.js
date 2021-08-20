53f6e08eac59f07667110bb81b85625ac2f8b8e3
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-08-21 04:12:12 +0530

    Manage admin dashboard

--- frontend/hms/src/Room_Management/ManageRoomsDashboard.js
+++ frontend/hms/src/Room_Management/ManageRoomsDashboard.js
@@ -3,6 +3,7 @@ import { useParams} from "react-router";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import '../CSS/Room_Management/ManageRoomsDashboard.css';
+import AdminSideNav from "../Admin/AdminSideNav";
 
 
 const ManageRoomsDashboard = () => {
@@ -34,19 +35,16 @@ const ManageRoomsDashboard = () => {
         getRoom();
     }, [Room]);
 
-
     return (
-        <div>
-            <link
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css"
-                rel='stylesheet' type='text/css' />
-            <br/><br/>
-            <div class="container-fluid">
-                <div class="row">
+        <div className="row1">
+            <div className="col-2"> <AdminSideNav/></div>
+
+            <div class="col-10">
+                <div class="">
 
                     <div class="col-12 col-sm-6 col-md-6">
-                        <a href="/AddRooms" class="btn btn-primary" role="button">
-                            <i class="fa fa-plus"></i>&nbsp;Add New Room
+                        <a className="btn btn-default foodPrices" href={"/"} >
+                            <i className="fa fa-arrow-left" style={{fontWeight: "bold"}}> Home</i>
                         </a>
                     </div>
                     <div className="form-group pull-right col-lg-4">
@@ -55,27 +53,30 @@ const ManageRoomsDashboard = () => {
                     <span className="counter pull-right"></span>
                     <br/> <br/><br/>
                 </div>
-                <div class="card" id="TableSorterCard">
+
                     <div class="card-header">
-                            <div className="form-group pull-right col-lg-4">
-                                    <button className="btn btn-primary" type="submit" id="fa">&nbsp;Generate Report</button>&nbsp;&nbsp;
-                        </div>
+
+                            <a href="/AddRooms" className="btn btn-primary" role="button">
+                                <i className="fa fa-plus"></i>&nbsp;Add New Room
+                            </a>
+                        <button className="btn btn-success btngenarate" type="submit" >Generate Report</button>
                     </div>
-                </div>
-                <div class="row">
+
+                <div class="row1">
                     <div class="col-12">
                         <div class="table-responsive">
-                            <table class="table  table-hover table-bordered table-striped table tablesorter" id="ipi-table">
+                            <table class="table1  table-hover table-bordered table-striped  tablesorter" id="ipi-table">
                                 <thead class="thead-dark">
                                 <tr>
                                     <th class="text-center">Room Type</th>
-                                    <th className="text-center col-lg-4">Room Image</th>
+                                    <th className="text-center col-lg-3">Room Image</th>
                                     <th class="text-center">Sleeps</th>
                                     <th className="text-center">Current Price</th>
-                                    <th className="text-center">Facilities</th>
-                                    <th className="text-center">Description</th>
-                                    <th class="text-center filter-false sorter-false col-lg-1">Actions</th>
+                                    <th className="text-center col-lg-2">Facilities</th>
+                                    <th className="text-center col-lg-2">Description</th>
+                                    <th class="text-center filter-false sorter-false ">Actions</th>
                                 </tr>
+
                                 </thead>
                                 <tbody class="text-center">
                                 {Room.map((Room) => {
@@ -87,7 +88,7 @@ const ManageRoomsDashboard = () => {
                                             <td>{Room.CurrentPrice}</td>
                                             <td>{Room.Facilities}</td>
                                             <td>{Room.Description}</td>
-
+                                            <br />
                                             <Link class="btn btn-primary" role="button" to={`/get/${Room._id}`}>
                                                 <em className="fa fa-eye"  id="icon"></em>
                                             </Link>
@@ -97,19 +98,12 @@ const ManageRoomsDashboard = () => {
                                             <a className="btn btn-danger" id="icon">
                                                 <em className="fa fa-trash"
                                                 onClick={()=>{if(window.confirm("Are you sure you want to delete this Room?")){deleteRoom(Room._id)};}} /></a>
+                                            <br /><br />
                                         </tr>
                                     );
                                 })}
                                 </tbody>
                             </table>
-                            {/*<button className="btn btn-primary">*/}
-                            {/*    <Link to="/">+ Back to Home</Link>*/}
-                            {/*</button>*/}
-                            <div className="col-12 col-sm-6 col-md-6">
-                                <a href="/" className="btn btn-primary" role="button">
-                                    <i className="fa fa-plus"></i>&nbsp;Back to Home
-                                </a>
-                            </div>
                             <br/>
                         </div>
                     </div>
