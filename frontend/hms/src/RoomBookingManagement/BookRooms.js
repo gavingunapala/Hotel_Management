5b880ce2dbcee46008d04372724e6bcc336133a0
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-09-18 01:48:34 +0530

    Book rooms & Book rooms Dashboard

--- frontend/hms/src/RoomBookingManagement/BookRooms.js
+++ frontend/hms/src/RoomBookingManagement/BookRooms.js
@@ -1,8 +1,51 @@
 import React, {useState} from 'react';
 import '../CSS/RoomBookingManagement/BookRooms.css';
 import img from "../Images/undraw_Sync_files_re_ws4c.png";
+import {useHistory} from 'react-router-dom';
+import axios from "axios";
+import ProgressBar from "../comps/ProgressBar.js";
+import '../CSS/ProgressBar/ProgressBar.css'
+
 
 const BookRooms = () => {
+    const history = useHistory();
+
+    const [RoomType, setRoomType] = useState("");
+    const [NoOfPeople, setNoOfPeople] = useState("");
+    const [CheckInDate, setCheckInDate] = useState("");
+    const [CheckOutDate, setCheckOutDate] = useState("");
+
+
+    const roomtypeSetter = (e) => {
+        setRoomType(e.target.value);
+    }
+    const noofpeopleSetter = (e) => {
+        setNoOfPeople(e.target.value);
+    }
+    const checkindateSetter = (e) => {
+        setCheckInDate(e.target.value);
+    }
+    const checkoutdateSetter = (e) => {
+        setCheckOutDate(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const newRoomBooking = {
+            RoomType: RoomType,
+            NoOfPeople: NoOfPeople,
+            CheckInDate: CheckInDate,
+            CheckOutDate: CheckOutDate
+
+        };
+        axios.post('http://localhost:8070/RoomBooking/add', newRoomBooking).then(() => {
+            alert("New Room Booked successfully!");
+            history.push('/RoomBookingDashboard');
+        }).catch((err) => {
+            alert(err);
+        })
+    }
+
     return (
         <div>
             <br></br>
@@ -25,7 +68,7 @@ const BookRooms = () => {
                                         <div><label for="type">Room Type</label>
                                             <select className="form-control"
                                                     name="type"
-                                                    id="type" >
+                                                    id="type" onChange={roomtypeSetter}>
                                                 <option>Choose</option>
                                                 <option>Premium Double Room</option>
                                                 <option>Cilantro Suite</option>
@@ -34,13 +77,13 @@ const BookRooms = () => {
                                                 <option>Superior King Room</option>
                                             </select>
                                         </div>
-                                        <div><label>No Of People</label><input id='left' className="form-control" type="number"  /></div>
-                                        <div><label>Check In Date</label><input className="form-control" type="date" /></div>
-                                        <div><label>Check Out Date</label><input className="form-control" type="date" /></div>
+                                        <div><label>No Of People</label><input id='left' className="form-control" type="number" onChange={noofpeopleSetter}  /></div>
+                                        <div><label>Check In Date</label><input className="form-control" type="date" onChange={checkindateSetter} /></div>
+                                        <div><label>Check Out Date</label><input className="form-control" type="date" onChange={checkoutdateSetter} /></div>
 
 
                                         <br/>
-                                        <button className="btn btn-primary" type="submit" id="addrooms" >&nbsp;Book</button>&nbsp;&nbsp;
+                                        <button className="btn btn-primary" type="submit" id="addrooms" onClick={onSubmit} >&nbsp;Book</button>&nbsp;&nbsp;
                                         <br/>
                                         <br/>
 
